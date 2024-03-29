b5fb3c3b4ecf306831a99de13f7fb81514af7f45
vvenate3 <vitor.venante@hotmail.com>
2024-03-29 13:09:27 -0300

    insercao da classe psicologo e ferramenta

--- ferramenta.py
+++ ferramenta.py
@@ -1,1 +1,12 @@
-#classe ferramenta
+from policial import Policial
+class Ferramenta:
+    idFerramenta                = ' '           #PRECISARIA DISSO MESMO?
+    DataRealizada               = ' '           ##2º ETAPA (relacionar com o /Policial.py)
+    ImgDeteccaoFace             = ' '           ##2º ETAPA Verificar a existencia da face
+    ImgDeteccao                 = ' '           #EXPRESSOES
+
+def reconhecerFace():                           ##2º Desenvolver Ferramenta
+    # DESCREVER MÉTODO
+
+def detectarExpressoes():                       ##2º Desenvolver Ferramenta
+    # DESCREVER MÉTODO
--- psicologo.py
+++ psicologo.py
@@ -1,1 +1,10 @@
-# CLASSE PSICOLOGO
+class Psicologo:
+    idPsicologo         = ' '
+    nomePsicologo       = ' '
+    CRP                 = ' '
+
+def entrevistar():
+    # DESCREVER MÉTODO
+
+def diagnosticar():                             ##2ºETAPA: RELACIONAR COM O RESULTADO
+    # DESCREVER MÉTODO
