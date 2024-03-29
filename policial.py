47f225b66a6e80b2bd07557c4a8c9eb85ad51b60
vvenate3 <vitor.venante@hotmail.com>
2024-03-29 12:00:18 -0300

    elaboracao classe policial.py

--- policial.py
+++ policial.py
@@ -1,1 +1,13 @@
-# CLASSE POLICIAL
+class Policial:
+    idPolicial      = '01'
+    dataCadastro    = '29/03/2024'
+    DataNascimento  = '10/07/2002'
+    sexo            = 'M'
+    imgPolicial     = ' ' #arquivo da imagem do policial                    ##2º ETAPA
+def cadastrarPolicial(idPolicial, dataCadastro, DataNascimento, sexo):
+
+def editarPolicial(idPolicial, dataCadastro, DataNascimento, sexo):
+
+def editarPolicial(idPolicial, dataCadastro, DataNascimento, sexo):
+
+def imgPolicial(): #arquivo de imagem do policial                           ##2º ETAPA
